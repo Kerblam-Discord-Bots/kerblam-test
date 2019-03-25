25075f50c92b0454add491471e26bd684e562aa0
kerblamdiscordbots <45296988+The78thdoctor@users.noreply.github.com>
2019-03-25 09:40:17 -0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,24 @@
+let {RichEmbed} = require("discord.js");
+module.exports.run = (client) => {
+    console.log(`
+    Bot Account: ${client.user.tag}
+    Bot ID: ${client.user.id}
+    Server Count: ${client.guilds.size}
+    Emoji Count: ${client.emojis.size}
+    Channel Count: ${client.channels.size}
+    User Count: ${client.users.size}
+    `);
+    client.user.setPresence({
+        status: "Online",
+        game: {
+         name: "Kerblam it| Join Kerblam's Support Server https://discord.gg/cmymsAY",
+         type: "PLAYING"
+       }
+    })
+  let embed = new RichEmbed()
+    .setAuthor(client.user.tag, client.user.displayAvatarURL)
+    .setColor(`#FF000`)
+    .setTitle(`Kerblam Online`)
+    .setTimestamp()
+    client.channels.get("521023970007449610").send(embed)
+}
